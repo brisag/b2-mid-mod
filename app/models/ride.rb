d5fe1002e2a578c444277ba31d823c9c1815ec40
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-22 11:19:47 -0700

    User story 2 with descending thrills

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,9 @@
 class Ride < ApplicationRecord
   has_many :mechanic_rides
   has_many :mechanics, through: :mechanic_rides
+
+  def self.sort_by_most_thrills
+    order(rating: :desc)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 
   resources :mechanics do
     resources :rides, controller: "mechanic_rides", only: [:create]
+  end
 end
