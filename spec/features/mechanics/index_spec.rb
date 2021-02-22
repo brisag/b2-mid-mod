3ba0f59e98976c3ac7fc0cf8d4b3d7aa64d921dd
Brisa Garcia <brisagarciaglz@gmail.com>
2021-02-22 10:03:32 -0700

    Finish User Story 1.  Need to fix capybara failure, TBD

--- spec/features/mechanics/index_spec.rb
+++ spec/features/mechanics/index_spec.rb
@@ -1,26 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
-  describe 'when i visit the mechanics index page' do
     before :each do
       @mechanic_1 = Mechanic.create!(name: "Anne", years: 6)
       @mechanic_2 = Mechanic.create!(name: "Bob", years: 4)
       @mechanic_3 = Mechanic.create!(name: "Carl", years: 8)
     end
 
+    describe 'when i visit the mechanics index page' do
     it 'I see a header saying “All Mechanics”' do
       visit '/mechanics'
         expect(page).to have_content("All Mechanics")
         save_and_open_page
+      end
 
-    it 'list of all mechanics anf its attributes' do
+    it 'list of all mechanics and its attributes' do
       expect(page).to have_content("#{@mechanic_1.name}")
       expect(page).to have_content("#{@mechanic_1.experience} years of experience")
       expect(page).to have_content("#{@mechanic_2.name}")
       expect(page).to have_content("#{@mechanic_2.experience} years of experience")
       expect(page).to have_content("#{@mechanic_3.name}")
       expect(page).to have_content("#{@mechanic_3.experience} years of experience")
-      end
+      
     end
   end
 end
